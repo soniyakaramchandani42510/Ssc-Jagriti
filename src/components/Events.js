67985749e5c79cb220ecd132d532569e5064bb12
Soniya Karamchandani <soniyaKaramchandani02@gmail.com>
2022-03-15 21:39:52 +0530

    updated

--- src/components/Events.js
+++ src/components/Events.js
@@ -4,33 +4,8 @@ import './Events.css'
 function Events() {
   return (
     <>
-    <div className="events">
-        <span className="competitions">
-          Competitions
-        </span>
-        <span className="guest">
-          Guest Talks
-        </span>
-        <p className='para'>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-           Quaerat ad iste ipsa omnis molestias, modi sunt iusto nisi
-            necessitatibus consequuntur ducimus laborum saepe cumque
-             alias unde non expedita molestiae excepturi.
-             </p>
-        </div>
-        <div>
-          <div>
-            <img id="images" src={require('../Assets/events.png')} alt="" />
-          </div>
-          <div className='over'>
-            <h6 className='overview'>Overview</h6>
-            <h6 className='lines'>Timeline</h6>
-            <h6 className='contacts'>Contacts</h6>
-          </div>
-        </div>
-
-
-
+  
+   
     </>
   )
 }
