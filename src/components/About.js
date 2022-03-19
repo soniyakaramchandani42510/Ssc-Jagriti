d34d016a1df5bfe5cd3f0ca299a3740bee3edde0
Kanika <kanika.bansal.met20@itbhu.ac.in>
2022-03-19 15:31:56 +0530

    changes

--- src/components/About.js
+++ src/components/About.js
@@ -15,6 +15,72 @@ function About() {
 		Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae reprehenderit nam rerum quas saepe. Consequatur quaerat fuga aliquam, quidem sapiente, nostrum assumenda consectetur sint, aliquid tenetur facere quis dignissimos dolorem!
 </p>
 </div>
+<div id="cover">
+  <div class="testimonial">
+
+  <div class="card" id='card2'>
+  <div class="layer"></div>
+
+  <div class="content">
+  <div class="image"><img src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/oscar_boy-128.png"       alt="avatar"/> 
+    </div>
+    <div class="details">
+      <h2>Someone Famous</h2> <br/>
+    </div>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+    
+         </div>
+</div>
+
+<div class="card" id='card2'>
+  <div class="layer"></div>
+
+  <div class="content">
+  <div class="image"><img src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/oscar_boy-128.png"       alt="avatar"/> 
+    </div>
+    <div class="details">
+      <h2>Someone Famous</h2> <br/>
+    </div>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+    
+         </div>
+</div>
+
+</div>
+</div>
+<div id="cover">
+  <div class="testimonial">
+
+    <div class="card" id='card2'>
+  <div class="layer"></div>
+
+  <div class="content">
+  <div class="image"><img src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/oscar_boy-128.png"       alt="avatar"/> 
+    </div>
+    <div class="details">
+      <h2>Someone Famous</h2> <br/>
+    </div>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+    
+         </div>
+</div>
+
+<div class="card" id='card2'>
+  <div class="layer"></div>
+
+  <div class="content">
+  <div class="image"><img src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/oscar_boy-128.png"       alt="avatar"/> 
+    </div>
+    <div class="details">
+      <h2>Someone Famous</h2> <br/>
+    </div>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+    
+         </div>
+</div>
+
+</div>
+</div>
 
 
 
