562bfc7e70272c1a13487772f72dc8e20fb92f8e
Aihab Umair <addisonhardley111@gmail.com>
2022-03-21 14:44:20 +0530

    Updated testimonial page

--- src/components/Testimonial/Testimonial.js
+++ src/components/Testimonial/Testimonial.js
@@ -14,13 +14,16 @@ function Testimonial() {
                 <img
                   className="photo"
                   src="https://picsum.photos/500/300/?image=10"
+                  alt="testimonial"
                 />
               </div>
               <div class="card_content card-color1">
-                <h2 class="card_title">Card Grid Layout</h2>
+                <h2 class="card_title">Toni Morrison</h2>
                 <p class="card_text">
-                  Demo of pixel perfect pure CSS simple responsive card grid
-                  layout
+                  If there’s a book that you want to read, but it hasn’t been
+                  written yet, then you must write it. If there’s a book that
+                  you want to read, but it hasn’t been written yet, then you
+                  must write it.
                 </p>
               </div>
             </div>
@@ -31,13 +34,15 @@ function Testimonial() {
                 <img
                   className="photo"
                   src="https://picsum.photos/500/300/?image=5"
+                  alt="testimonial"
                 />
               </div>
               <div class="card_content card-color2">
-                <h2 class="card_title">Card Grid Layout</h2>
+                <h2 class="card_title">Theodore Roosevelt</h2>
                 <p class="card_text">
-                  Demo of pixel perfect pure CSS simple responsive card grid
-                  layout
+                  There is nothing brilliant or outstanding in my record, except
+                  perhaps this one thing. I do the things I believe ought to be
+                  done. And when I make up my mind to do a thing, I act.
                 </p>
               </div>
             </div>
@@ -49,13 +54,14 @@ function Testimonial() {
                 <img
                   className="photo"
                   src="https://picsum.photos/500/300/?image=2"
+                  alt="testimonial"
                 />
               </div>
               <div class="card_content card-color3">
-                <h2 class="card_title">Card Grid Layout</h2>
+                <h2 class="card_title">Jen Sincero</h2>
                 <p class="card_text">
-                  Demo of pixel perfect pure CSS simple responsive card grid
-                  layout
+                  If you’re serious about changing your life, you’ll find a way.
+                  If you’re not, you’ll find an excuse. If you’re serious about changing your life, you’ll find a way. If you’re not, you’ll find an excuse.
                 </p>
               </div>
             </div>
